a2021a02b322eb79859c37d2fcdad209f39e99b7
Clemens Neubauer <cn-tools@gmx.at>
2023-01-20 09:42:07 +0100

    Use encodeURIComponent

--- 000-basic_script.js
+++ 000-basic_script.js
@@ -4,11 +4,9 @@ var lBotID = "<enter-your-botID>";
 var lChatID = "<enter-your-chatID>";
 
 var lPkgName = package.getName();
-var lTxt = "myJD2: YEHAAA - '" + lPkgName + "' has been completed."
-
-lTxt = lTxt.replace('ä', 'ae').replace('ö', 'oe').replace('ü', 'ue').replace('ß', 'ss');
-lTxt = lTxt.replace('Ä', 'AE').replace('Ö', 'OE').replace('Ü', 'UE');
-lTxt = lTxt.replace(/ /g, '%20').split('\n').join('%0A');
+var lTxt = "myJD2: YEHAAA - '" + lPkgName + "' wurde fertiggestellt."
+lTxt = lTxt.split('\n').join('%0A');
+lTxt = encodeURIComponent(lTxt);
 
 var lURL = "https://api.telegram.org/bot" + lBotID + "/sendMessage?chat_id=" + lChatID + "&text=" + lTxt;
 
