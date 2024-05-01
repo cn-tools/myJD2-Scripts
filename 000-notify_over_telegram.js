fe71a99f51727d218fff65f2e7e097f351de9e6e
Clemens Neubauer <cn-tools@gmx.at>
2024-05-01 15:24:49 +0200

    Update and rename 000-basic_script.js to 000-notify_over_telegram.js

--- 000-notify_over_telegram.js
+++ 000-notify_over_telegram.js
@@ -1,10 +1,12 @@
 // Add your script here. You can use the available API properties and methods.
+// find infos here: https://stackoverflow.com/questions/66357558/back-to-line-in-api-calls-with-fetch-for-telegram-bot
 
 var lBotID = "<enter-your-botID>";
 var lChatID = "<enter-your-chatID>";
 
 var lPkgName = package.getName();
-var lTxt = "myJD2: YEHAAA - '" + lPkgName + "' wurde fertiggestellt."
+var lTxt = "myJD2: YEHAAA - '" + lPkgName + "' has been completed."
+
 lTxt = lTxt.split('\n').join('%0A');
 lTxt = encodeURIComponent(lTxt);
 
